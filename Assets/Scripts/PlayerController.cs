a7c950fbe61ee934c214beb73821aa9604dd5087
Fernando Valcazara <fernandovalcazara@gmail.com>
2025-09-30 20:27:45 +0200

    Improvements for extra points

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,33 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Prevents car from tipping over sideways
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
     void Update()
     {
+        // Get the player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        Vector3 move = transform.forward * forwardInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
 
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        Quaternion turn = Quaternion.Euler(0, horizontalInput * turnSpeed * Time.fixedDeltaTime, 0);
+        rb.MoveRotation(rb.rotation * turn);
     }
 }
